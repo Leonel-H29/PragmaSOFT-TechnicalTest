1396218ad51229b519c344908465258a9cf1934e
Leonel Herrera <48606307+Leonel-H29@users.noreply.github.com>
2023-10-19 14:21:21 -0300

    Creo el formulario de Login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SeriesComponent } from './components/series/series.component';
+import { LoginComponent } from './components/login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: SeriesComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/components/login/login/login.component.ts
+++ client/src/app/components/login/login/login.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user';
+import { DbSettingsService } from 'src/app/service/db-settings.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  IsLogged = false;
+  IsLoginFail = false;
+  loginUser!: User;
 
+  credentials = {
+    host: '',
+    puerto: 0,
+    nombre_base_de_datos: '',
+    usuario: '',
+    contrasena: '',
+  };
+  error = '';
+
+  constructor(private db: DbSettingsService, private router: Router) {}
+
+  ngOnInit(): void {
+    if (this.db.getToken()) {
+      this.IsLogged = true;
+      this.IsLoginFail = false;
+    }
+  }
+
+  onLogin(): void {
+    this.loginUser = new User(
+      this.credentials.host,
+      this.credentials.puerto,
+      this.credentials.nombre_base_de_datos,
+      this.credentials.usuario,
+      this.credentials.contrasena
+    );
+    console.log(this.loginUser);
+    this.db.Login(this.loginUser).subscribe(
+      (data) => {
+        this.IsLogged = true;
+        this.IsLoginFail = false;
+        this.db.setToken('true');
+        this.router.navigate(['/admin']);
+      },
+      (err) => {
+        this.IsLogged = false;
+        this.IsLoginFail = true;
+        this.error = err.error.error;
+        alert(this.error);
+
+        //Swal.fire('Fallo la operacion');
+        //alert(this.errorMsj);
+        //console.log(this.errorMsj);
+      }
+    );
+  }
 }
