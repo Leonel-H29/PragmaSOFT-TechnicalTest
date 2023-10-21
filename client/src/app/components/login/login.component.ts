03603ff03b6a1d61a3eeac9dbcec9e138fd8f6c3
Leonel Herrera <48606307+Leonel-H29@users.noreply.github.com>
2023-10-21 10:29:58 -0300

    Modifico la implementacion del formulario de Login

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { min } from 'rxjs';
 import { User } from 'src/app/model/user';
 import { DbSettingsService } from 'src/app/service/db-settings.service';
+import { esCampoRequerido } from 'src/app/utils/validations-util';
 
 @Component({
   selector: 'app-login',
@@ -13,50 +16,81 @@ export class LoginComponent {
   IsLoginFail = false;
   loginUser!: User;
 
-  credentials = {
-    host: '',
-    puerto: 0,
-    nombre_base_de_datos: '',
-    usuario: '',
-    contrasena: '',
-  };
-  error = '';
+  formulario!: FormGroup;
+  isFormValid?: boolean;
 
-  constructor(private db: DbSettingsService, private router: Router) {}
+  constructor(
+    private db: DbSettingsService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     if (this.db.getToken()) {
       this.IsLogged = true;
       this.IsLoginFail = false;
     }
+    this.formulario = this.fb.group({
+      host: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(260),
+        ],
+      ],
+      port: [0, Validators.min(0)],
+      database_name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(260),
+        ],
+      ],
+      user: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(260),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(260),
+        ],
+      ],
+    });
+  }
+
+  validarInputs(control: any) {
+    return esCampoRequerido(control);
   }
 
   onLogin(): void {
-    this.loginUser = new User(
-      this.credentials.host,
-      this.credentials.puerto,
-      this.credentials.nombre_base_de_datos,
-      this.credentials.usuario,
-      this.credentials.contrasena
-    );
-    console.log(this.loginUser);
-    this.db.Login(this.loginUser).subscribe(
-      (data) => {
-        this.IsLogged = true;
-        this.IsLoginFail = false;
-        this.db.setToken('true');
-        this.router.navigate(['/admin']);
-      },
-      (err) => {
-        this.IsLogged = false;
-        this.IsLoginFail = true;
-        this.error = err.error.error;
-        alert(this.error);
-
-        //Swal.fire('Fallo la operacion');
-        //alert(this.errorMsj);
-        //console.log(this.errorMsj);
-      }
-    );
+    if (this.formulario.valid) {
+      const credentials = this.formulario.value;
+      console.log(credentials);
+      this.db.Login(credentials).subscribe(
+        (data) => {
+          alert('Conexion con la base de datos exitosa!');
+          this.IsLogged = true;
+          this.IsLoginFail = false;
+          this.db.setToken('true');
+          this.router.navigate(['/admin']);
+        },
+        (err) => {
+          alert('Error al conectar a la base de datos');
+          this.IsLogged = false;
+          this.IsLoginFail = true;
+        }
+      );
+      // Enviar nuevoRegistro al servicio o realizar la acción correspondiente
+      //this.modal.close('success');
+    }
   }
 }
