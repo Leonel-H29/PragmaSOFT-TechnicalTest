333e09f3ba16865da30912163c5fbb000aebd833
Leonel-H29 <48606307+Leonel-H29@users.noreply.github.com>
2023-10-22 16:29:30 -0300

    Implemento formulario de busqueda para los datos de la tabla

--- client/src/app/components/series/list-series/list-series.component.ts
+++ client/src/app/components/series/list-series/list-series.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Serie } from 'src/app/model/series';
 import { SeriesService } from 'src/app/service/series.service';
 import { NewSeriesComponent } from '../new-series/new-series.component';
@@ -13,12 +13,14 @@ import { LogoutComponent } from '../../logout/logout.component';
   templateUrl: './list-series.component.html',
   styleUrls: ['./list-series.component.css'],
 })
-export class ListSeriesComponent {
+export class ListSeriesComponent implements OnInit {
   list_series: Serie[] = new Array<Serie>();
+  list_filter: Serie[] = new Array<Serie>();
   isLogged = false;
   hasPermission = false;
   registroSeleccionado!: Serie;
   mensajeAnulado = false;
+  showSearchInput = false;
 
   modalUpdate = UpdateSeriesComponent;
   modalAnulacion = EstadoSeriesComponent;
@@ -115,5 +117,36 @@ export class ListSeriesComponent {
         console.log('Error');
       }
     });
+  }
+
+  toggleSearchInput() {
+    this.showSearchInput = !this.showSearchInput;
+    // Si se muestra el input de búsqueda, restaurar la lista completa
+    if (this.showSearchInput) {
+      this.list_filter = [...this.list_series];
+    }
+  }
+  filterData(query: string) {
+    if (!query) {
+      // Si la búsqueda está vacía, restaurar la lista completa
+      this.GetSeries(); // Esto debe cargar la lista completa nuevamente
+      return;
+    }
+
+    query = query.toLowerCase(); // Convertir a minúsculas para una búsqueda insensible a mayúsculas
+
+    // Filtrar las series por ID, título y descripción
+    this.list_series = this.list_filter.filter(
+      (serie) =>
+        //serie.id_serie?.toString().toLowerCase().includes(query) ||
+        serie.titulo.toLowerCase().includes(query) ||
+        serie.descripcion.toLowerCase().includes(query) ||
+        //serie.fecha_estreno.toDateString().toLowerCase().includes(query) ||
+        //serie.estrellas.toString().toLowerCase().includes(query) ||
+        serie.genero.toLowerCase().includes(query) ||
+        //serie.precio_alquiler.toString().toLowerCase().includes(query) ||
+        //serie.atp.toString().toLowerCase().includes(query) ||
+        serie.estado.toLowerCase().includes(query)
+    );
   }
 }
--- client/src/app/components/series/search-series/search-series.component.ts
+++ client/src/app/components/series/search-series/search-series.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-series',
   templateUrl: './search-series.component.html',
-  styleUrls: ['./search-series.component.css']
+  styleUrls: ['./search-series.component.css'],
 })
-export class SearchSeriesComponent {
+export class SearchSeriesComponent implements OnInit {
+  @Output() search = new EventEmitter<string>();
 
+  onInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.search.emit(inputElement.value);
+  }
+
+  ngOnInit(): void {}
 }
